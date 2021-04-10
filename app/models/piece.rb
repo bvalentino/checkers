53507e411b4b280df1f8e3f19c075c8e80835a48
Bruno Valentino <brunovalentino@gmail.com>
2021-04-10 12:49:41 -0400

    Pieces next moves as kings

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -14,20 +14,49 @@ class Piece
     self.row == row && self.column == column
   end
 
-  def next_moves
+  def next_moves(vertical: nil, horizontal: nil)
+    if vertical.nil? && horizontal.nil?
+      return HORIZONTALS.product(VERTICALS).collect do |direction|
+        next_moves(vertical: direction[0], horizontal: direction[1])
+      end.flatten
+    end
+
     moves = []
+    return moves unless can_move_vertically?(vertical)
 
-    # try diagonal left
-    if (board.get_piece(next_row, column - 1).nil? rescue false)
-      moves << { row: next_row, column: column - 1 }
+    row_delta = vertical == :bottom ? 1 : -1
+    column_delta = horizontal == :right ? 1 : -1
+
+    jumps = 0
+    while jumps < max_jumps
+      jumps += 1
+
+      begin
+        target_row = row + jumps * row_delta
+        target_column = column + jumps * column_delta
+        piece_at_position = board.get_piece(target_row, target_column)
+
+        if piece_at_position.nil?
+          moves << { row: target_row, column: target_column }
+        elsif piece_at_position.player != player
+          # we can overtake this piece if the next position is free
+          target_row = row + (jumps + 1) * row_delta
+          target_column = column + (jumps + 1) * column_delta
+          piece_at_next_position = board.get_piece(target_row, target_column)
+
+          if piece_at_next_position.nil?
+            moves << { row: target_row, column: target_column }
+          end
+
+          break
+        else
+          break # we can't overtake our own pieces
+        end
+      rescue
+        # position is out of bounds
+        # nothing to do here
+      end
     end
-
-    # try diagonal right
-    if (board.get_piece(next_row, column + 1).nil? rescue false)
-      moves << { row: next_row, column: column + 1 }
-    end
-
-    # TODO: king moves
 
     moves
   end
@@ -37,12 +66,19 @@ class Piece
   end
 
   private
+    HORIZONTALS = %i[top bottom]
+    VERTICALS = %i[left right]
 
-    def direction
-      player == 'x' ? -1 : 1
+    def can_move_vertically?(direction)
+      return true if is_king
+      return :top if player == 'x'
+
+      :bottom
     end
 
-    def next_row(skip_rows = 0)
-      row + (skip_rows + 1) * direction
+    def max_jumps
+      return board.size - 1 if is_king
+
+      1
     end
 end
